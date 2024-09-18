cb53b1a0fe88d18a67e3c14e5f1aaaaaa279449b
sazzadrupak <sazzad@oployeelabs.com>
2024-09-19 00:00:43 +0300

    initialize project to add react test library

---
+++ web/vite.config.ts
@@ -0,0 +1,12 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true,
+    port: 3000,
+    strictPort: true,
+  },
+});
