12659330f20a284970d796e1a46bf5c21c84dd38
sazzadrupak <sazzad@oployeelabs.com>
2024-09-19 23:35:44 +0300

    Testing data fetching: product details component

---
+++ web/tests/mocks/data.ts
@@ -0,0 +1,5 @@
+export const products = [
+  { id: 1, name: 'Laptop', price: 10 },
+  { id: 2, name: 'Shampoo', price: 9 },
+  { id: 3, name: 'Gardening Gloves', price: 8 },
+];
--- web/tests/mocks/handlers.ts
+++ web/tests/mocks/handlers.ts
@@ -1,4 +1,5 @@
 import { http, HttpResponse } from 'msw';
+import { products } from './data';
 
 export const handlers = [
   http.get('/categories', () => {
@@ -9,10 +10,15 @@ export const handlers = [
     ])
   }),
   http.get('/products', () => {
-    return HttpResponse.json([
-      { id: 1, name: 'Laptop' },
-      { id: 2, name: 'Shampoo' },
-      { id: 3, name: 'Gardening Gloves' },
-    ])
+    return HttpResponse.json(products)
+  }),
+  http.get('/products/:id', ({ params }) => {
+    const { id } = params;
+
+    const product = products.find((product) => product.id === Number(id));
+    if (!product) {
+      return new HttpResponse(null, { status: 404, statusText: 'Not Found' });
+    }
+    return HttpResponse.json(product);
   }),
 ]
