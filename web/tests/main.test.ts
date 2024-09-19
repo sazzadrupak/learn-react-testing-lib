6d51a6e0fc2c61a46614adee27b9901b9485966a
sazzadrupak <sazzad@oployeelabs.com>
2024-09-19 23:38:51 +0300

    Install Faker.js

--- web/tests/main.test.ts
+++ web/tests/main.test.ts
@@ -1,10 +1,11 @@
-import { it, expect, describe } from 'vitest'
+import { it, expect, describe } from 'vitest';
+import { faker } from '@faker-js/faker';
 
 describe('group', () => {
-  it('should work', async () => {
-    const response = await fetch('/categories');
-    const data = await response.json();
-    console.log(data);
-    expect(data).toHaveLength(3);
+  it('should work', () => {
+    console.log({
+      name: faker.commerce.productName(),
+      price: faker.commerce.price(),
+    })
   })
 })
