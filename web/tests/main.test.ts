d2a4da2a4c63b8f2ebc158fd1b901d2cefcdd4b6
sazzadrupak <sazzad@oployeelabs.com>
2024-09-19 22:16:38 +0300

    Setting up mock service worker

--- web/tests/main.test.ts
+++ web/tests/main.test.ts
@@ -1,7 +1,10 @@
 import { it, expect, describe } from 'vitest'
 
 describe('group', () => {
-  it('should work', () => {
-    expect(1).toBeTruthy()
+  it('should work', async () => {
+    const response = await fetch('/categories');
+    const data = await response.json();
+    console.log(data);
+    expect(data).toHaveLength(3);
   })
 })
