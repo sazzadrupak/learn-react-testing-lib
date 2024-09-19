a91cd70a9040e9b56e82888c60e9045d525ac5b5
sazzadrupak <sazzad@oployeelabs.com>
2024-09-19 10:24:50 +0300

    Testing user account component

---
+++ web/tests/components/UserAccount.test.tsx
@@ -0,0 +1,31 @@
+import { render, screen } from '@testing-library/react';
+import { User } from '../../src/entities';
+import UserAccount from '../../src/components/UserAccount';
+
+describe('UserAccount', () => {
+  it('should render user name', () => {
+    const user: User = { id: 1, name: 'Rupak' };
+
+    render(<UserAccount user={user} />);
+
+    expect(screen.getByText(user.name)).toBeInTheDocument();
+  });
+
+  it('should render edit button if user is admin', () => {
+    const user: User = { id: 1, name: 'Rupak', isAdmin: true };
+
+    render(<UserAccount user={user} />);
+
+    const button = screen.getByRole('button');
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveTextContent(/edit/i);
+  });
+
+  it('should not render edit button if user is not admin', () => {
+    const user: User = { id: 1, name: 'Rupak' };
+
+    render(<UserAccount user={user} />);
+
+    expect(screen.queryByRole('button')).not.toBeInTheDocument();
+  });
+});
