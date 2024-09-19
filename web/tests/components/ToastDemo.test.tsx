c754e4bcd086a7d7062f1088ba3a66a9778f2f42
sazzadrupak <sazzad@oployeelabs.com>
2024-09-19 15:07:22 +0300

    Test toast demo (machMedia error handle)

---
+++ web/tests/components/ToastDemo.test.tsx
@@ -0,0 +1,23 @@
+import { render, screen } from '@testing-library/react';
+import { Toaster } from 'react-hot-toast';
+
+import ToastDemo from '../../src/components/ToastDemo';
+import userEvent from '@testing-library/user-event';
+
+describe('ToastDemo', () => {
+  it('should render a toast', async () => {
+    render(
+      <>
+        <ToastDemo />
+        <Toaster />
+      </>
+    );
+
+    const button = screen.getByRole('button');
+    const user = userEvent.setup();
+    await user.click(button);
+
+    const toast = await screen.findByText(/success/i);
+    expect(toast).toBeInTheDocument();
+  });
+});
