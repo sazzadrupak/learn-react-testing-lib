9f0d7053f997fd31510862d4aef210958946eea4
sazzadrupak <sazzad@oployeelabs.com>
2024-09-20 09:33:45 +0300

    Test error in product list component

--- web/tests/components/ProductList.test.tsx
+++ web/tests/components/ProductList.test.tsx
@@ -32,4 +32,12 @@ describe('ProductList', () => {
     const message = await screen.findByText(/no products/i);
     expect(message).toBeInTheDocument();
   });
+
+  it('should render error message when there is an error', async () => {
+    server.use(http.get('/products', () => HttpResponse.error()));
+    render(<ProductList />);
+
+    const message = await screen.findByText(/error/i);
+    expect(message).toBeInTheDocument();
+  });
 });
